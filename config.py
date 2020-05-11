056dfc76bcd2d1cd3043cb185a36bd863c99d7fb
Prem Ganeshkumar <ganeprem@gmail.com>
2020-05-11 19:17:14 -0400

    hello world flask app

--- config.py
+++ config.py
@@ -3,4 +3,7 @@
 import os
 
 class Config(object):
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+	CONSUMER_KEY = os.environ.get('CONSUMER_KEY') or 'you-will-never-guess'
+	CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET') or 'you-will-never-guess'
+	ACCESS_KEY = os.environ.get('ACCESS_KEY') or 'you-will-never-guess'
+	ACCESS_SECRET = os.environ.get('ACCESS_SECRET') or 'you-will-never-guess'
