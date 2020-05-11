4adc485198fbe966f366fb3d351a699158408d03
Prem Ganeshkumar <ganeprem@gmail.com>
2020-05-11 19:28:59 -0400

    All the env vars we need. Add Procfile so Heroku knows how to run this web app

--- config.py
+++ config.py
@@ -3,7 +3,9 @@
 import os
 
 class Config(object):
-	CONSUMER_KEY = os.environ.get('CONSUMER_KEY') or 'you-will-never-guess'
-	CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET') or 'you-will-never-guess'
-	ACCESS_KEY = os.environ.get('ACCESS_KEY') or 'you-will-never-guess'
-	ACCESS_SECRET = os.environ.get('ACCESS_SECRET') or 'you-will-never-guess'
+	TWITTER_CONSUMER_KEY = os.environ.get('CONSUMER_KEY') or 'you-will-never-guess'
+	TWITTER_CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET') or 'you-will-never-guess'
+	TWITTER_ACCESS_KEY = os.environ.get('ACCESS_KEY') or 'you-will-never-guess'
+	TWITTER_ACCESS_SECRET = os.environ.get('ACCESS_SECRET') or 'you-will-never-guess'
+
+	DATABASE_URL = os.environ.get('DATABASE_URL') or 'you-will-never-guess'
