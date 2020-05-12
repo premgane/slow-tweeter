6711bd896d7225de89cc8d29531722302e145f88
Prem <prem@rogue.local>
2020-05-12 15:35:53 -0400

    Clean up pyflakes linting issues

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+
 
 class PostingForm(FlaskForm):
     tweet = StringField('tweet', validators=[DataRequired()])
