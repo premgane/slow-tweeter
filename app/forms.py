128dfd38cc4a5416043d9b2b4f4bf0cdeac4bb56
Prem Ganeshkumar <ganeprem@gmail.com>
2020-05-11 20:32:40 -0400

    Grab the right imports

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
-from wtforms import StringField, SubmitField
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 class PostingForm(FlaskForm):
     tweet = StringField('tweet', validators=[DataRequired()])
